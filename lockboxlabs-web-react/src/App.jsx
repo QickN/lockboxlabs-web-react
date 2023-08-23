a6e0eb1dbcee70eb036e5808f61496c365bf60c3
QickN <nickquam38@gmail.com>
2023-08-23 12:48:48 -0700

    Updated footer header and main text

---
+++ lockboxlabs-web-react/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="ourTeam.html">our team</a></li>
+            <li><a href="support.html">support</a></li>
+            <li><a href="synapse.html">privacy policies</a></li>
+            <li><a href="https://qickn.github.io/Antonyms" target='_blank'>antonyms</a></li>
+          </ul>
+        </nav>
+        </header>
+      <h1>lockboxlabs</h1>
+      <div className="card">
+        <button>
+          our work
+        </button>
+      </div>
+      <footer>&copy; Copyright lockboxlabs, LLC.</footer>
+      
+    </>
+  )
+}
+
+export default App
